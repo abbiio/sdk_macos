161c484a390b1bf128955e74df4683475f034ce5
jenkins <jenkins@buildserver.walkme.local>
2019-05-05 18:09:15 +0300

    Version 0.4.2

--- WalkMe.framework/Versions/A/Headers/WalkMe.h
+++ WalkMe.framework/Versions/A/Headers/WalkMe.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT const unsigned char WalkMeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WalkMe/PublicHeader.h>
 #import <WalkMe/WalkMeSDK.h>
-#import <WalkMeEditor/WalkMeStartOptions.h>
--- WalkMeEditor.podspec
+++ WalkMeEditor.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "WalkMeEditor"
-  s.version      = "0.4.1"
+  s.version      = "0.4.2"
   s.summary      = "WalkMe Editor SDK for macOS"
   s.homepage     = "http://walkme.com"
   s.license      = "Commercial"
